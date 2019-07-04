ed744bbe8093a5149c65116f505d837d9dd27f28
Immánuel! <immanuelfactor+github@gmail.com>
2019-07-04 15:58:48 +0200

    Map the i386 platform to the x86 binary

--- scripts/install_update_linux.sh
+++ scripts/install_update_linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 GITHUB_LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/jesseduffield/lazydocker/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
-GITHUB_FILE="lazydocker_${GITHUB_LATEST_VERSION//v/}_$(uname -s)_$(uname -m | sed 's/aarch64/arm64/').tar.gz"
+GITHUB_FILE="lazydocker_${GITHUB_LATEST_VERSION//v/}_$(uname -s)_$(uname -m | sed 's/aarch64/arm64/;s/i386/x86/').tar.gz"
 GITHUB_URL="https://github.com/jesseduffield/lazydocker/releases/download/${GITHUB_LATEST_VERSION}/${GITHUB_FILE}"
 
 wget -O lazydocker.tar.gz $GITHUB_URL
