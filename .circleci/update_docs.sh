018e151e1fb5855497b1818edab84a42b7dda9b1
Jesse Duffield <jessedduffield@gmail.com>
2019-07-07 12:45:53 +1000

    update docs when merging to master

---
+++ .circleci/update_docs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ex
+
+# see if we have a new cheatsheet
+# if other docs end up being generated automatically we can chuck in the relevant scripts here
+go run scripts/generate_cheatsheet.go
+
+# commit and push if we have a change
+if [[ -z $(git status -s -- docs/*) ]]; then
+  echo "no changes to commit in the docs directory"
+  exit 0
+fi
+
+echo "committing updated docs"
+
+git config user.name "lazydocker bot"
+git config user.email "jessedduffield@gmail.com"
+
+git add docs/*
+git commit -m "update docs"
+git push -u origin HEAD
