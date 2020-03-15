60640d4fe446e41839e8d0551d86b4484cffb419
Dawid Dziurla <dawidd0811@gmail.com>
2020-03-15 19:02:45 +0100

    install_update_linux: allow specifying directory
    Closes #208

--- scripts/install_update_linux.sh
+++ scripts/install_update_linux.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# allow specifying different destination directory
+DIR="${DIR:-"/usr/local/bin"}"
+
 # map different architecture variations to the available binaries
 ARCH=$(uname -m)
 case $ARCH in
@@ -17,5 +20,5 @@ GITHUB_URL="https://github.com/jesseduffield/lazydocker/releases/download/${GITH
 # install/update the local binary
 curl -L -o lazydocker.tar.gz $GITHUB_URL
 tar xzvf lazydocker.tar.gz lazydocker
-sudo mv -f lazydocker /usr/local/bin/
+sudo mv -f lazydocker "$DIR"
 rm lazydocker.tar.gz
