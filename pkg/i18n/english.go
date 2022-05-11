a100caa0d95544e96f4d9bc178ab597743f378f1
Jesse Duffield <jessedduffield@gmail.com>
2022-05-11 18:17:41 +1000

    use sdk to inspect containers

--- pkg/i18n/english.go
+++ pkg/i18n/english.go
@@ -21,6 +21,7 @@ type TranslationSet struct {
 	ErrorOccurred                              string
 	ConnectionFailed                           string
 	UnattachableContainerError                 string
+	WaitingForContainerInfo                    string
 	CannotAttachStoppedContainerError          string
 	CannotAccessDockerSocketError              string
 	CannotKillChildError                       string
@@ -115,6 +116,7 @@ func englishSet() TranslationSet {
 		ErrorOccurred:                     "An error occurred! Please create an issue at https://github.com/jesseduffield/lazydocker/issues",
 		ConnectionFailed:                  "connection to docker client failed. You may need to restart the docker client",
 		UnattachableContainerError:        "Container does not support attaching. You must either run the service with the '-it' flag or use `stdin_open: true, tty: true` in the docker-compose.yml file",
+		WaitingForContainerInfo:           "Cannot proceed until docker gives us more information about the container. Please retry in a few moments.",
 		CannotAttachStoppedContainerError: "You cannot attach to a stopped container, you need to start it first (which you can actually do with the 'r' key) (yes I'm too lazy to do this automatically for you) (pretty cool that I get to communicate one-on-one with you in the form of an error message though)",
 		CannotAccessDockerSocketError:     "Can't access docker socket at: unix:///var/run/docker.sock\nRun lazydocker as root or read https://docs.docker.com/install/linux/linux-postinstall/",
 		CannotKillChildError:              "Waited three seconds for child process to stop. There may be an orphan process that continues to run on your system.",
