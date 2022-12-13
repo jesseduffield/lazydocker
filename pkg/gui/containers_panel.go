6127e0df7d833973694f80191c92b4fe308a766c
glendsoza <glendsouza23@gmail.com>
2022-12-13 19:14:41 +0530

    Added the translation set for detach from container shortcut message
    Fix the linting again

--- pkg/gui/containers_panel.go
+++ pkg/gui/containers_panel.go
@@ -409,7 +409,7 @@ func (gui *Gui) handleContainerAttach(g *gocui.Gui, v *gocui.View) error {
 		return gui.createErrorPanel(err.Error())
 	}
 
-	return gui.runSubprocessWithMessage(c, "By default, to detach from the container press ctrl-p then ctrl-q")
+	return gui.runSubprocessWithMessage(c, gui.Tr.DetachFromContainerShortCut)
 }
 
 func (gui *Gui) handlePruneContainers() error {
